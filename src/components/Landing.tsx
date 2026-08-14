ed06bf4686631c6779935ece56725ff31fa0e733
asadmobashshir <mobaasad5813@gmail.com>
2026-08-14 16:25:17 +0530

    Update Landing.tsx

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,7 +9,6 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              <Mobashshir></Mobashshir>
               <br />
               <span>Asad</span>
             </h1>
